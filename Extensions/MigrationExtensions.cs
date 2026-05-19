342d96339b393b093496ae0fb6f5bd9a56070665
Khánh Hưng <khanhhungzm@gmail.com>
2026-05-19 10:53:05 +0700

    chore: gate startup migrations and centralize test exclusions

--- Extensions/MigrationExtensions.cs
+++ Extensions/MigrationExtensions.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 
 namespace Platform.Api.Extensions;
 
@@ -9,6 +11,15 @@ public static class MigrationExtensions
     public static async Task ApplyMigrationsAsync<TContext>(this IApplicationBuilder app) where TContext : DbContext
     {
         using var scope = app.ApplicationServices.CreateScope();
+        var environment = scope.ServiceProvider.GetRequiredService<IHostEnvironment>();
+        var configuration = scope.ServiceProvider.GetRequiredService<IConfiguration>();
+        var applyOnStartup = environment.IsDevelopment() || configuration.GetValue<bool>("Database:ApplyMigrationsOnStartup");
+
+        if (!applyOnStartup)
+        {
+            return;
+        }
+
         var context = scope.ServiceProvider.GetRequiredService<TContext>();
         await context.Database.MigrateAsync();
     }
